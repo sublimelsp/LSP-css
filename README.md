45cd33098c9e2ca7c9bc8eb24b6bbb2f5b2c60de
Predrag Nikolic <predrag.nikolic@codetri.be>
2019-07-27 12:07:50 +0200

    📦 NEW: Simplify installing of the plugin

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
 # LSP-css
 CSS, SCSS, LESS support for Sublime's LSP plugin.
 
-1. Install [LSP](https://packagecontrol.io/packages/LSP) and `LSP-css` from Package Control.
-2. From the command palette run `LSP: Setup CSS server`.
-3. Restart Sublime.
-
-If you use Sass install [Sass](https://packagecontrol.io/packages/Sass).
+* Install [LSP](https://packagecontrol.io/packages/LSP) and `LSP-css` from Package Control. If you use Sass install [Sass](https://packagecontrol.io/packages/Sass).
 If you use Less install [LESS](https://packagecontrol.io/packages/LESS).
-
-Server related issues report [here](https://github.com/vscode-langservers/vscode-css-languageserver).
-
-> Note: On Mac, if you have problems running `LSP: Setup CSS server`, install [Fix Mac Path](https://packagecontrol.io/packages/Fix%20Mac%20Path), and optionally set the `additional_path_items` in your user preferences.
+* Restart Sublime.
--- plugin.py
+++ plugin.py
@@ -1,104 +1,81 @@
 import shutil
-
+import os
 import sublime
-import sublime_plugin
+
 from LSP.plugin.core.handlers import LanguageHandler
 from LSP.plugin.core.settings import ClientConfig, LanguageConfig
 
-default_name = 'css'
-server_package_name = 'vscode-css-languageserver-bin'
 
-default_config = ClientConfig(
-    name=default_name,
-    binary_args=[
-        "css-languageserver",
-        "--stdio"
-    ],
-    tcp_port=None,
-    enabled=True,
-    init_options=dict(),
-    settings=dict(),
-    env=dict(),
-    languages=[
-        LanguageConfig(
-            'css',
-            ['source.css'],
-            ["Packages/CSS/CSS.sublime-syntax"]
-        ),
-        LanguageConfig(
-            'scss',
-            ['source.scss'],
-            ["Packages/Sass/Syntaxes/SCSS.sublime-syntax"]
-        ),
-        LanguageConfig(
-            'less',
-            ['source.less'],
-            ['Packages/LESS/Syntaxes/LESS.sublime-syntax']
+package_path = os.path.dirname(__file__)
+server_path = os.path.join(package_path, 'node_modules', 'vscode-css-languageserver-bin', 'cssServerMain.js')
+
+
+def plugin_loaded():
+    print('LSP-css: Server {} installed.'.format('is' if os.path.isfile(server_path) else 'is not' ))
+
+    if not os.path.isdir(os.path.join(package_path, 'node_modules')):
+        # install server if no node_modules
+        print('LSP-css: Installing server.')
+        sublime.active_window().run_command(
+            "exec", {
+                "cmd": [
+                    "npm",
+                    "install",
+                    "--verbose",
+                    "--prefix",
+                    package_path
+                ]
+            }
         )
-    ]
-)
-
-# Dependencies that needs to be installed for the server to work
-dependencies = ['node', 'css-languageserver']
+        sublime.message_dialog('LSP-css\n\nRestart sublime after the server has been installed successfully.')
 
 
-def is_installed(dependency):
-    return shutil.which(dependency) is not None
-
-
-class LspCssSetupCommand(sublime_plugin.WindowCommand):
-    def is_visible(self):
-        if not is_installed('node') or not is_installed('css-languageserver'):
-            return True
-        return False
-
-    def run(self):
-        if not is_installed('node'):
-            sublime.message_dialog(
-                "Please install Node.js before running setup."
-            )
-            return
-
-        if not is_installed('css-languageserver'):
-            should_install = sublime.ok_cancel_dialog(
-                "css-languageserver was not in the PATH.\nInstall {} globally now?".format(
-                    server_package_name)
-            )
-            if should_install:
-                self.window.run_command(
-                    "exec", {
-                        "cmd": [
-                            "npm",
-                            "install",
-                            "--verbose",
-                            "-g",
-                            server_package_name
-                        ]
-                    })
-        else:
-            sublime.message_dialog(
-                "{} is already installed".format(server_package_name)
-            )
+def is_node_installed():
+    return shutil.which('node') is not None
 
 
 class LspCssPlugin(LanguageHandler):
-    def __init__(self):
-        self._name = default_name
-        self._config = default_config
-
     @property
     def name(self) -> str:
-        return self._name
+        return 'lsp-css'
 
     @property
     def config(self) -> ClientConfig:
-        return self._config
+        return ClientConfig(
+            name='lsp-css',
+            binary_args=[
+                'node',
+                server_path,
+                '--stdio'
+            ],
+            tcp_port=None,
+            enabled=True,
+            init_options=dict(),
+            settings=dict(),
+            env=dict(),
+            languages=[
+                LanguageConfig(
+                    'css',
+                    ['source.css'],
+                    ["Packages/CSS/CSS.sublime-syntax"]
+                ),
+                LanguageConfig(
+                    'scss',
+                    ['source.scss'],
+                    ["Packages/Sass/Syntaxes/SCSS.sublime-syntax"]
+                ),
+                LanguageConfig(
+                    'less',
+                    ['source.less'],
+                    ['Packages/LESS/Syntaxes/LESS.sublime-syntax']
+                )
+            ]
+        )
 
     def on_start(self, window) -> bool:
-        for dependency in dependencies:
-            if not is_installed(dependency):
-                sublime.status_message('Run: LSP: Setup CSS server')
-                return False
+        if not is_node_installed():
+            sublime.status_message('Please install Node.js for the CSS Language Server to work.')
+            return False
         return True
 
     def on_initialized(self, client) -> None:
