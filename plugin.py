25fb2bd9c0d847d9b809029a2c0bce561a4e5572
Predrag <idmpepe@gmail.com>
2019-07-29 00:05:12 +0200

    Use sublime.packages_path()

--- plugin.py
+++ plugin.py
@@ -1,37 +1,62 @@
 import shutil
 import os
 import sublime
+import threading
+import subprocess
 
 from LSP.plugin.core.handlers import LanguageHandler
 from LSP.plugin.core.settings import ClientConfig, LanguageConfig
 
 
-package_path = os.path.dirname(__file__)
-server_path = os.path.join(package_path, 'node_modules', 'vscode-css-languageserver-bin', 'cssServerMain.js')
-
-
 def plugin_loaded():
+    package_path = os.path.join(sublime.packages_path(), 'LSP-css')
+    server_path = os.path.join(package_path, 'node_modules', 'vscode-css-languageserver-bin', 'cssServerMain.js')
     print('LSP-css: Server {} installed.'.format('is' if os.path.isfile(server_path) else 'is not' ))
 
+    # install the node_modules if not installed
     if not os.path.isdir(os.path.join(package_path, 'node_modules')):
-        # install server if no node_modules
-        print('LSP-css: Installing server.')
-        sublime.active_window().run_command(
-            "exec", {
-                "cmd": [
-                    "npm",
-                    "install",
-                    "--verbose",
-                    "--prefix",
-                    package_path
-                ]
-            }
+        # this will be called only when the plugin gets:
+        # - installed for the first time,
+        # - or when updated on package control
+        logAndShowMessage('LSP-css: Installing server.')
+
+        runCommand(
+            onCommandDone,
+            ["npm", "install", "--verbose", "--prefix", package_path]
         )
-        sublime.message_dialog('LSP-css\n\nRestart sublime after the server has been installed successfully.')
+
+
+def onCommandDone():
+    logAndShowMessage('LSP-css: Server installed.')
+
+
+def runCommand(onExit, popenArgs):
+    """
+    Runs the given args in a subprocess.Popen, and then calls the function
+    onExit when the subprocess completes.
+    onExit is a callable object, and popenArgs is a list/tuple of args that
+    would give to subprocess.Popen.
+    """
+    def runInThread(onExit, popenArgs):
+        try:
+            subprocess.check_call(popenArgs)
+            onExit()
+        except subprocess.CalledProcessError as error:
+            logAndShowMessage('LSP-css: Error while installing the server.', error)
+        return
+    thread = threading.Thread(target=runInThread, args=(onExit, popenArgs))
+    thread.start()
+    # returns immediately after the thread starts
+    return thread
 
 
 def is_node_installed():
     return shutil.which('node') is not None
+
+
+def logAndShowMessage(msg, additional_logs=None):
+    print(msg, '\n', additional_logs) if additional_logs else print(msg)
+    sublime.active_window().status_message(msg)
 
 
 class LspCssPlugin(LanguageHandler):
@@ -41,6 +66,8 @@ class LspCssPlugin(LanguageHandler):
 
     @property
     def config(self) -> ClientConfig:
+        package_path = os.path.join(sublime.packages_path(), 'LSP-css')
+        server_path = os.path.join(package_path, 'node_modules', 'vscode-css-languageserver-bin', 'cssServerMain.js')
         return ClientConfig(
             name='lsp-css',
             binary_args=[
@@ -57,7 +84,7 @@ class LspCssPlugin(LanguageHandler):
                 LanguageConfig(
                     'css',
                     ['source.css'],
-                    ["Packages/CSS/CSS.sublime-syntax"]
+                    ["Packages/CSS/CSS.sublime-syntax", "Packages/CSS3/CSS3.sublime-syntax"]
                 ),
                 LanguageConfig(
                     'scss',
