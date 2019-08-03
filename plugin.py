22046de82c7bf0e12a61fff597f6463f6903dccd
Predrag Nikolic <predrag.nikolic@codetri.be>
2019-08-03 18:48:00 +0200

    Fix server not installing on windows

--- plugin.py
+++ plugin.py
@@ -8,9 +8,11 @@ from LSP.plugin.core.handlers import LanguageHandler
 from LSP.plugin.core.settings import ClientConfig, LanguageConfig
 
 
+package_path = os.path.dirname(__file__)
+server_path = os.path.join(package_path, 'node_modules', 'vscode-css-languageserver-bin', 'cssServerMain.js')
+
+
 def plugin_loaded():
-    package_path = os.path.join(sublime.packages_path(), 'LSP-css')
-    server_path = os.path.join(package_path, 'node_modules', 'vscode-css-languageserver-bin', 'cssServerMain.js')
     print('LSP-css: Server {} installed.'.format('is' if os.path.isfile(server_path) else 'is not' ))
 
     # install the node_modules if not installed
@@ -22,7 +24,7 @@ def plugin_loaded():
 
         runCommand(
             onCommandDone,
-            ["npm", "install", "--verbose", "--prefix", package_path]
+            ["npm", "install", "--verbose", "--prefix", package_path, package_path]
         )
 
 
@@ -39,7 +41,10 @@ def runCommand(onExit, popenArgs):
     """
     def runInThread(onExit, popenArgs):
         try:
-            subprocess.check_call(popenArgs)
+            if sublime.platform() == 'windows':
+                subprocess.check_call(popenArgs, shell=True)
+            else:
+                subprocess.check_call(popenArgs)
             onExit()
         except subprocess.CalledProcessError as error:
             logAndShowMessage('LSP-css: Error while installing the server.', error)
@@ -66,8 +71,6 @@ class LspCssPlugin(LanguageHandler):
 
     @property
     def config(self) -> ClientConfig:
-        package_path = os.path.join(sublime.packages_path(), 'LSP-css')
-        server_path = os.path.join(package_path, 'node_modules', 'vscode-css-languageserver-bin', 'cssServerMain.js')
         return ClientConfig(
             name='lsp-css',
             binary_args=[
