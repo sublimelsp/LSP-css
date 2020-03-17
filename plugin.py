1bac1d5c2e34aa56530b00abfe0cd47083a6b324
Jack Cherng <jfcherng@gmail.com>
2020-03-18 04:28:58 +0800

    refactor: migrate settings to flatter structure
    Just like how it's done in https://github.com/sublimelsp/LSP-json/pull/8
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -33,19 +33,40 @@ class LspCssPlugin(LanguageHandler):
 
     @property
     def config(self) -> ClientConfig:
-        settings = sublime.load_settings(SETTINGS_FILENAME)
-        client_configuration = settings.get('client')
-        if client_configuration is None:
-            client_configuration = {}
-
         # Calling setup() also here as this might run before `plugin_loaded`.
         # Will be a no-op if already ran.
         # See https://github.com/sublimelsp/LSP/issues/899
         server.setup()
 
-        default_configuration = self.get_default_configuration()
-        default_configuration.update(client_configuration)
+        configuration = self.migrate_and_read_configuration()
+
+        default_configuration = {
+            'enabled': True,
+            'command': ['node', server.binary_path, '--stdio'],
+        }
+
+        default_configuration.update(configuration)
+
         return read_client_config('lsp-css', default_configuration)
+
+    def migrate_and_read_configuration(self) -> dict:
+        settings = {}
+        loaded_settings = sublime.load_settings(SETTINGS_FILENAME)
+
+        if loaded_settings:
+            if loaded_settings.has('client'):
+                client = loaded_settings.get('client')
+                loaded_settings.erase('client')
+                # Migrate old keys
+                for key in client:
+                    loaded_settings.set(key, client[key])
+                sublime.save_settings(SETTINGS_FILENAME)
+
+            # Read configuration keys
+            for key in ['languages', 'initializationOptions', 'settings']:
+                settings[key] = loaded_settings.get(key)
+
+        return settings
 
     def on_start(self, window) -> bool:
         if not is_node_installed():
@@ -55,123 +76,3 @@ class LspCssPlugin(LanguageHandler):
 
     def on_initialized(self, client) -> None:
         pass   # extra initialization here.
-
-    def get_default_configuration(self) -> dict:
-        return {
-            "enabled": True,
-            "command": ['node', server.binary_path, '--stdio'],
-            "languages": [
-                {
-                    "languageId": "css",
-                    "scopes": ["source.css"],
-                    "syntaxes": [
-                        "Packages/CSS/CSS.sublime-syntax",
-                        "Packages/CSS3/CSS3.sublime-syntax",
-                    ],
-                },
-                {
-                    "languageId": "scss",
-                    "scopes": ["source.scss"],
-                    "syntaxes": ["Packages/Sass/Syntaxes/SCSS.sublime-syntax"],
-                },
-                {
-                    "languageId": "less",
-                    "scopes": ["source.less"],
-                    "syntaxes": ["Packages/LESS/Syntaxes/LESS.sublime-syntax"],
-                },
-            ],
-            "initializationOptions": {
-                # @see https://github.com/sublimelsp/LSP-css/pull/2#discussion_r393881421
-                "dataPaths": [],
-            },
-            "settings": {
-                "css": {
-                    "customData": [],
-                    "completion": {
-                        "triggerPropertyValueCompletion": True,
-                        "completePropertyWithSemicolon": True,
-                    },
-                    "validate": True,
-                    "lint": {
-                        "compatibleVendorPrefixes": "ignore",
-                        "vendorPrefix": "warning",
-                        "duplicateProperties": "ignore",
-                        "emptyRules": "warning",
-                        "importStatement": "ignore",
-                        "boxModel": "ignore",
-                        "universalSelector": "ignore",
-                        "zeroUnits": "ignore",
-                        "fontFaceProperties": "warning",
-                        "hexColorLength": "error",
-                        "argumentsInColorFunction": "error",
-                        "unknownProperties": "warning",
-                        "validProperties": [],
-                        "ieHack": "ignore",
-                        "unknownVendorSpecificProperties": "ignore",
-                        "propertyIgnoredDueToDisplay": "warning",
-                        "important": "ignore",
-                        "float": "ignore",
-                        "idSelector": "ignore",
-                        "unknownAtRules": "warning",
-                    },
-                },
-                "scss": {
-                    "completion": {
-                        "triggerPropertyValueCompletion": True,
-                        "completePropertyWithSemicolon": True,
-                    },
-                    "validate": True,
-                    "lint": {
-                        "compatibleVendorPrefixes": "ignore",
-                        "vendorPrefix": "warning",
-                        "duplicateProperties": "ignore",
-                        "emptyRules": "warning",
-                        "importStatement": "ignore",
-                        "boxModel": "ignore",
-                        "universalSelector": "ignore",
-                        "zeroUnits": "ignore",
-                        "fontFaceProperties": "warning",
-                        "hexColorLength": "error",
-                        "argumentsInColorFunction": "error",
-                        "unknownProperties": "warning",
-                        "validProperties": [],
-                        "ieHack": "ignore",
-                        "unknownVendorSpecificProperties": "ignore",
-                        "propertyIgnoredDueToDisplay": "warning",
-                        "important": "ignore",
-                        "float": "ignore",
-                        "idSelector": "ignore",
-                        "unknownAtRules": "warning",
-                    },
-                },
-                "less": {
-                    "completion": {
-                        "triggerPropertyValueCompletion": True,
-                        "completePropertyWithSemicolon": True,
-                    },
-                    "validate": True,
-                    "lint": {
-                        "compatibleVendorPrefixes": "ignore",
-                        "vendorPrefix": "warning",
-                        "duplicateProperties": "ignore",
-                        "emptyRules": "warning",
-                        "importStatement": "ignore",
-                        "boxModel": "ignore",
-                        "universalSelector": "ignore",
-                        "zeroUnits": "ignore",
-                        "fontFaceProperties": "warning",
-                        "hexColorLength": "error",
-                        "argumentsInColorFunction": "error",
-                        "unknownProperties": "warning",
-                        "validProperties": [],
-                        "ieHack": "ignore",
-                        "unknownVendorSpecificProperties": "ignore",
-                        "propertyIgnoredDueToDisplay": "warning",
-                        "important": "ignore",
-                        "float": "ignore",
-                        "idSelector": "ignore",
-                        "unknownAtRules": "warning",
-                    },
-                },
-            },
-        }
