56e884f9cd998d3a8d432575a7b827ad254249cd
Rafał Chłodnicki <rchl2k@gmail.com>
2020-05-02 18:19:58 +0200

    Switch to NpmClientHandler

--- plugin.py
+++ plugin.py
@@ -1,78 +1,16 @@
-import shutil
 import os
-import sublime
-
-from LSP.plugin.core.handlers import LanguageHandler
-from LSP.plugin.core.settings import ClientConfig, read_client_config
-from lsp_utils import ServerNpmResource
-
-PACKAGE_NAME = 'LSP-css'
-SETTINGS_FILENAME = 'LSP-css.sublime-settings'
-SERVER_DIRECTORY = 'vscode-css'
-SERVER_BINARY_PATH = os.path.join(SERVER_DIRECTORY, 'out', 'cssServerMain.js')
-
-server = ServerNpmResource(PACKAGE_NAME, SERVER_DIRECTORY, SERVER_BINARY_PATH)
+from lsp_utils import NpmClientHandler
 
 
 def plugin_loaded():
-    server.setup()
+    LspCssPlugin.setup()
 
 
 def plugin_unloaded():
-    server.cleanup()
+    LspCssPlugin.cleanup()
 
 
-def is_node_installed():
-    return shutil.which('node') is not None
-
-
-class LspCssPlugin(LanguageHandler):
-    @property
-    def name(self) -> str:
-        return PACKAGE_NAME.lower()
-
-    @property
-    def config(self) -> ClientConfig:
-        # Calling setup() also here as this might run before `plugin_loaded`.
-        # Will be a no-op if already ran.
-        # See https://github.com/sublimelsp/LSP/issues/899
-        server.setup()
-
-        configuration = self.migrate_and_read_configuration()
-
-        default_configuration = {
-            'enabled': True,
-            'command': ['node', server.binary_path, '--stdio'],
-        }
-
-        default_configuration.update(configuration)
-
-        return read_client_config('lsp-css', default_configuration)
-
-    def migrate_and_read_configuration(self) -> dict:
-        settings = {}
-        loaded_settings = sublime.load_settings(SETTINGS_FILENAME)
-
-        if loaded_settings:
-            if loaded_settings.has('client'):
-                client = loaded_settings.get('client')
-                loaded_settings.erase('client')
-                # Migrate old keys
-                for key in client:
-                    loaded_settings.set(key, client[key])
-                sublime.save_settings(SETTINGS_FILENAME)
-
-            # Read configuration keys
-            for key in ['languages', 'initializationOptions', 'settings']:
-                settings[key] = loaded_settings.get(key)
-
-        return settings
-
-    def on_start(self, window) -> bool:
-        if not is_node_installed():
-            sublime.status_message('Please install Node.js for the CSS Language Server to work.')
-            return False
-        return server.ready
-
-    def on_initialized(self, client) -> None:
-        pass   # extra initialization here.
+class LspCssPlugin(NpmClientHandler):
+    package_name = __package__
+    server_directory = 'vscode-css'
+    server_binary_path = os.path.join(server_directory, 'out', 'cssServerMain.js')
