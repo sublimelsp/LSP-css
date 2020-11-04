f3d76e4feda1bcfb2bd146c42728063dad6339f2
Rafał Chłodnicki <rchl2k@gmail.com>
2020-11-04 10:58:33 +0100

    Install server outside of the cache directory

--- plugin.py
+++ plugin.py
@@ -14,3 +14,7 @@ class LspCssPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'language-server'
     server_binary_path = os.path.join(server_directory, 'out', 'node', 'cssServerMain.js')
+
+    @classmethod
+    def install_in_cache(cls) -> bool:
+        return False
