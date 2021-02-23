0113fae237e4ec6dca651840938326eef81bb3f9
Rafał Chłodnicki <rchl2k@gmail.com>
2021-02-24 00:10:56 +0100

    cleanup

--- plugin.py
+++ plugin.py
@@ -1,4 +1,3 @@
-from LSP.plugin.core.typing import Mapping, Any, Callable
 from lsp_utils import NpmClientHandler
 import os
 
@@ -15,14 +14,3 @@ class LspCssPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'language-server'
     server_binary_path = os.path.join(server_directory, 'out', 'node', 'cssServerMain.js')
-
-    def on_pre_server_command(self, command: Mapping[str, Any], done_callback: Callable[[], None]) -> bool:
-        if command['command'] == 'editor.action.triggerSuggest':
-            session = self.weaksession()
-            if session:
-                view = session.window.active_view()
-                if view and view.is_valid():
-                    view.run_command("auto_complete")
-                    done_callback()
-                    return True
-        return False
